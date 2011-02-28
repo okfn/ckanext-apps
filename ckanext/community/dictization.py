6ee3143a34a8b0cc1d434acae12939a1eae7a913
Wayne Witzel III <wwitzel3@gmail.com>
2011-02-28 04:53:19 -0500

    one() will throw if not found, no need for the if check

--- ckanext/community/dictization.py
+++ ckanext/community/dictization.py
@@ -8,14 +8,13 @@ def application_dict_from_form(params):
     
 def application_details(application_name):
     app = Session.query(Application).filter_by(name=application_name).one()
-    if app:
-        return dict(
-            id=app.id,
-            name=app.name,
-            description=app.description,
-            url=app.url,
-            developed_by=app.developed_by,
-            submitter=app.submitter,
-            extras=app.extras,
-            tags=[tag.name for tag in app.tags]
-        )
+    return dict(
+        id=app.id,
+        name=app.name,
+        description=app.description,
+        url=app.url,
+        developed_by=app.developed_by,
+        submitter=app.submitter,
+        extras=app.extras,
+        tags=[tag.name for tag in app.tags]
+    )
