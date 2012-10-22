13f84364bb28fbd3f489fa1f4902eba832b067e6
Sean Hammond <seanhammond@lavabit.com>
2012-10-22 13:40:39 +0200

    Scrape images with right ID and size
    Change ckanext-apps-scraper.py to save image files with the image's
    application_id as filename, because the application_ids are what the
    ckanext-apps-migrate command uses as ids for the Related Items it
    creates.
    
    Also scrape the images at the right size for the CKAN 2.0 theme.

--- ckanext-apps-scraper.py
+++ ckanext-apps-scraper.py
@@ -8,13 +8,13 @@ You need access to the site's database to get the IDs of all the images.
 Get a file containing a list of your ckanext-apps image IDs from your
 PostgreSQL database with this command:
 
-    psql -d {DATABASE} -h {HOST} -U {USER} -c 'SELECT id FROM application_image;' -o {FILENAME} -t
+    psql -d {DATABASE} -h {HOST} -U {USER} -c 'SELECT id, application_id FROM application_image;' -o {FILENAME} -t
 
 Replace {DATABASE}, {HOST}, {USER} and {FILENAME} with your database name,
 the hostname of the machine your database server is running on, your database
 username, and the filename to write to. Example:
 
-    psql -d pdeu -h dbserver -U pdeu -c 'SELECT id FROM application_image;' -o image_ids -t
+    psql -d pdeu -h dbserver -U pdeu -c 'SELECT id, application_id FROM application_image;' -o image_ids -t
 
 Then feed the image IDs file to this scraper script with this command:
 
@@ -31,12 +31,19 @@ import commands
 base_url = sys.argv[1]
 if not base_url.endswith('/'):
     base_url = base_url + '/'
-for image_id in open(sys.argv[2], 'r').readlines():
-    stripped = image_id.strip()
-    if not stripped:
+for line in open(sys.argv[2], 'r').readlines():
+    if not line.strip():
         continue
-    url = "{base_url}app/image/{image_id}".format(base_url=base_url,
-            image_id=stripped)
-    cmd = "wget -nc '{0}'".format(url)
+    image_id, application_id = line.split('|')
+    image_id = image_id.strip()
+    if not image_id:
+        continue
+    application_id = application_id.strip()
+    if not application_id:
+        continue
+    url = "{base_url}app/image/{image_id}@203x126".format(base_url=base_url,
+            image_id=image_id)
+    cmd = "wget -nc -O '{application_id}.png' '{url}'".format(
+            application_id=application_id, url=url)
     status, output = commands.getstatusoutput(cmd)
     print output
